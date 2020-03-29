14f667650ebe68c45786119a65f8d0878dc4919b
weichao666 <510390985@qq.com>
2020-03-29 19:36:28 +0800

    10.SpringMVC

---
+++ springmvcserverC/src/test/java/servicecomb/springmvcserverc/java/training/classtest/A.java
@@ -0,0 +1,20 @@
+package servicecomb.springmvcserverc.java.training.classtest;
+
+public class A {
+    public static String a = printStr("父类的静态变量");
+    public String s = printStr("父类的非静态变量");
+    static {
+        printStr("父类的静态代码块");
+    }
+    {
+        printStr("父类的非静态代码块");
+    }
+    public A() {
+        printStr("父类的构造方法");
+    }
+
+    public static String printStr(String str) {
+        System.out.println(str);
+        return str;
+    }
+}
